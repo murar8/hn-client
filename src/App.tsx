ce4e426837cfac6e2d60a8a68f063f5a212f4479
Lorenzo Murarotto <lnzmrr@gmail.com>
2021-08-08 13:24:19 +0200

    clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { Box, ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import { Header } from "./components/Header";
 
 function App() {
   return (
-    <ChakraProvider>
+    <React.StrictMode>
       <BrowserRouter>
-        <Box minW="xs">
-          <Header />
-        </Box>
         <Switch>
           <Redirect exact from="/" to="/best" />
           <Route path="/best">Best</Route>
@@ -17,7 +12,7 @@ function App() {
           <Route path="/top">Top</Route>
         </Switch>
       </BrowserRouter>
-    </ChakraProvider>
+    </React.StrictMode>
   );
 }
 
