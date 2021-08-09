df3434b0fe78fa6d5ea04d3fce49abbae774b66b
Lorenzo Murarotto <lnzmrr@gmail.com>
2021-08-09 19:35:43 +0200

    allow usage of render prop

--- src/components/NamedRoute.test.tsx
+++ src/components/NamedRoute.test.tsx
@@ -1,4 +1,3 @@
-import {} from "@material-ui/core";
 import "@testing-library/jest-dom/extend-expect";
 import { render, waitFor } from "@testing-library/react";
 import { createMemoryHistory } from "history";
@@ -6,18 +5,51 @@ import { HelmetProvider } from "react-helmet-async";
 import { Router, Switch } from "react-router-dom";
 import { NamedRoute } from "./NamedRoute";
 
-it("should update the window title based on the current path", async () => {
+afterEach(() => {
+  document.getElementsByTagName("html")[0].innerHTML = "";
+});
+
+function renderWithContext(ui: React.ReactElement, options?: Omit<RenderOptions, "">) {
   const history = createMemoryHistory();
 
-  render(
-    <HelmetProvider>
-      <Router history={history}>
-        <Switch>
-          <NamedRoute name="Foo" path="/foo" />
-          <NamedRoute name="Bar" path="/bar" />
-        </Switch>
-      </Router>
-    </HelmetProvider>
+  const result = render(ui, {
+    wrapper: ({ children }) => (
+      <HelmetProvider>
+        <Router history={history}>
+          <Switch>{children}</Switch>
+        </Router>
+      </HelmetProvider>
+    ),
+  });
+
+  return { history, ...result };
+}
+
+it("should update the window title based on the current path when using children", async () => {
+  const { history } = renderWithContext(
+    <>
+      <NamedRoute name="Foo" path="/foo">
+        Foo
+      </NamedRoute>
+      <NamedRoute name="Bar" path="/bar">
+        Bar
+      </NamedRoute>
+    </>
+  );
+
+  await waitFor(() => expect(window.document.title).toEqual(""));
+
+  history.push("/foo");
+
+  await waitFor(() => expect(document.title).toEqual("Foo"));
+});
+
+it("should update the window title based on the current path when using a render function", async () => {
+  const { history } = renderWithContext(
+    <>
+      <NamedRoute name="Foo" path="/foo" render={() => "Foo"} />
+      <NamedRoute name="Bar" path="/bar" render={() => "Bar"} />
+    </>
   );
 
   await waitFor(() => expect(window.document.title).toEqual(""));
--- src/components/NamedRoute.tsx
+++ src/components/NamedRoute.tsx
@@ -1,14 +1,39 @@
 import { Helmet } from "react-helmet-async";
 import { Route, RouteProps } from "react-router-dom";
 
+type TitleProps = { name: string };
+
+function Title({ name, ...props }: TitleProps) {
+  return (
+    <Helmet {...props}>
+      <title>{name}</title>
+    </Helmet>
+  );
+}
+
 export type NamedRouteProps = RouteProps & { name: string };
 
-export function NamedRoute({ name, children, ...props }: NamedRouteProps) {
+export function NamedRoute({ name, children, render, ...props }: NamedRouteProps) {
+  if (render) {
+    return (
+      <Route
+        render={
+          render &&
+          ((props) => (
+            <>
+              <Title name={name} />
+              {render(props)}
+            </>
+          ))
+        }
+        {...props}
+      ></Route>
+    );
+  }
+
   return (
     <Route {...props}>
-      <Helmet>
-        <title>{name}</title>
-      </Helmet>
+      <Title name={name} />
       {children}
     </Route>
   );
