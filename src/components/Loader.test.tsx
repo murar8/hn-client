f13c7e58ccad920e37e25f7d758371470e1172b7
Lorenzo Murarotto <lnzmrr@gmail.com>
2021-09-05 16:40:38 +0200

    add loader smoke test

---
+++ src/components/Loader.test.tsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import { Loader } from "./Loader";
+
+it("renders a spinner", async () => {
+  render(<Loader />);
+  expect(screen.getByText("Loading...")).toBeVisible();
+});
