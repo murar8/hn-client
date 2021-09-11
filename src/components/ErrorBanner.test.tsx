74ec0bad594cc7bed06d3e0101dc026aa9900d7b
Lorenzo Murarotto <lnzmrr@gmail.com>
2021-09-11 17:08:16 +0200

    add onRetry prop

--- src/components/ErrorBanner.test.tsx
+++ src/components/ErrorBanner.test.tsx
@@ -1,10 +1,25 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { ErrorBanner } from "./ErrorBanner";
 
-it("displays the error message", async () => {
-  render(<ErrorBanner error={new Error("Could not break the sound barrier.")} />);
-  expect(screen.getByText("Could not break the sound barrier.")).toBeVisible();
+it("displays the error string representation if the error is an object", async () => {
   render(<ErrorBanner error="Could not reach the speed of light." />);
-  expect(screen.getByText("Could not reach the speed of light.")).toBeVisible();
+  expect(screen.getByText(/Could not reach the speed of light./)).toBeVisible();
+});
+
+it("displays the error message if the error is an Error instance", async () => {
+  render(<ErrorBanner error={new Error("Could not break the sound barrier.")} />);
+  expect(screen.getByText(/Could not break the sound barrier./)).toBeVisible();
+});
+
+it("shows a retry button when the onRetry prop is specified", async () => {
+  const onRetry = jest.fn();
+  render(<ErrorBanner error={new Error("Could not break the sound barrier.")} onRetry={onRetry} />);
+  const retryButton = screen.getByText("Retry");
+
+  expect(retryButton).toBeVisible();
+
+  fireEvent.click(retryButton);
+
+  expect(onRetry).toHaveBeenCalledTimes(1);
 });
--- src/components/ErrorBanner.tsx
+++ src/components/ErrorBanner.tsx
@@ -1,26 +1,44 @@
-import { ChakraProps, Heading, Icon, Stack, Text, useColorModeValue, VStack } from "@chakra-ui/react";
-import { useMemo } from "react";
+import { Button, ChakraProps, HStack, Icon, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import { MouseEventHandler, useMemo } from "react";
 import { FaExclamationTriangle } from "react-icons/fa";
 
 export type ErrorBannerProps = ChakraProps & {
   error: any;
+  onRetry?: MouseEventHandler;
 };
 
-export function ErrorBanner({ error, ...props }: ErrorBannerProps) {
+export function ErrorBanner({ error, onRetry, ...props }: ErrorBannerProps) {
   const message = useMemo(() => (error instanceof Error ? error.message : String(error)), [error]);
   const bgColor = useColorModeValue("red.500", "red.300");
+  const buttonBorderColor = useColorModeValue("gray.800", "gray.200");
 
   return (
-    <VStack bgColor={bgColor} spacing={2} alignItems="center" {...props}>
-      <Stack direction={{ base: "column", sm: "row" }} spacing={4} justifyContent="center" alignItems="center">
-        <Icon as={FaExclamationTriangle} boxSize={12} />
-        <Heading size="md" textAlign="center">
-          An error occured
-        </Heading>
-      </Stack>
-      <Text fontSize="md" textAlign="center" overflowWrap="anywhere">
-        {message}
-      </Text>
-    </VStack>
+    <Stack
+      direction={{ base: "column", md: "row" }}
+      bgColor={bgColor}
+      spacing={8}
+      alignItems="center"
+      justifyContent="space-between"
+      p={4}
+      {...props}
+    >
+      <HStack spacing={4} justifyContent="flex-start" alignItems="center">
+        <Icon as={FaExclamationTriangle} boxSize={10} />
+        <Text fontSize="md" overflowWrap="anywhere">
+          An error occured: {message}
+        </Text>
+      </HStack>
+      {onRetry !== undefined && (
+        <Button
+          alignSelf="flex-end"
+          variant="outline"
+          borderColor={buttonBorderColor}
+          minW="fit-content"
+          onClick={onRetry}
+        >
+          Retry
+        </Button>
+      )}
+    </Stack>
   );
 }
