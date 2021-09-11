d297e070f6464cd77e74518482a9cdcf7930430b
Lorenzo Murarotto <lnzmrr@gmail.com>
2021-09-11 16:16:44 +0200

    better tests

--- src/components/ShortLink.test.tsx
+++ src/components/ShortLink.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { ShortLink } from "./ShortLink";
 
 function Component() {
@@ -9,4 +9,18 @@ function Component() {
 it("renders a link", async () => {
   render(<Component />);
   expect(screen.getByText("github.com").closest("a")).toHaveProperty("href", "https://github.com/HackerNews/API");
+});
+
+it("does not propagate click events to the parent element", async () => {
+  const onClick = jest.fn();
+
+  render(
+    <div onClick={onClick}>
+      <Component />
+    </div>
+  );
+
+  fireEvent.click(screen.getByText("github.com"));
+
+  expect(onClick).not.toHaveBeenCalled();
 });
